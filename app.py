07e8e55664a18eac60e739446bf8281aad0ba332
Gagan K <gagan.k@gnani.ai>
2026-04-21 14:45:11 +0530

    app.py

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
 from reviewer import review_code
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {
+        "request": request
+    })
 
 
 @app.post("/review", response_class=HTMLResponse)
 async def review(request: Request, code: str = Form(...)):
 
-    print("USER INPUT:", code)  # DEBUG
-
     result = review_code(code)
 
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "review": result
+        "review": result,
+        "code": code
     })
-print("working")
